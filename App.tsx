f1e789bb0f2c62cf400cd6515f7214e19cca1c58
Edrick <10529706+edrickleong@users.noreply.github.com>
2022-08-23 20:56:35 +0800

    feat: add tailwind

--- App.tsx
+++ App.tsx
@@ -1,18 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<HomeScreen/>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        "./screens/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    // ...
+};
